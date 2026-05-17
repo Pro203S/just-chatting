f3dd68886a6cc9e8bfe2823d2b5790290f755911
Pro203S <prosirea2501@gmail.com>
2026-05-18 06:34:16 +0900

    회원탈퇴 만듬

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -7,6 +7,7 @@ import { useEffect, useRef, useState } from 'react';
 import REST from '@/src/modules/rest';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import Dialog, { DialogButton } from '@/src/components/dialog';
 
 function convertImageToBase64(file: File) {
     return new Promise<string>((resolve, reject) => {
@@ -28,6 +29,11 @@ export default function Page() {
 
     const [source, setSource] = useState<string>();
 
+    const [dialogTitle, setDialogTitle] = useState("");
+    const [dialogDesc, setDialogDesc] = useState("");
+    const [dialogButtons, setDialogButtons] = useState<DialogButton[]>([]);
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     const opacity = useSpringValue(1, {
         "config": {
             "duration": 480,
@@ -40,7 +46,7 @@ export default function Page() {
             const r = await REST<APIUser, APIError>("/api/users/me");
             if (!r.success) {
                 alert(r.data.message);
-                return;
+                return router.back();
             }
 
             setSession(r.data);
@@ -50,6 +56,13 @@ export default function Page() {
     if (!session) return null;
 
     return <>
+        <Dialog
+            title={dialogTitle}
+            description={dialogDesc}
+            buttons={dialogButtons}
+            open={dialogOpen}
+            onCancel={() => setDialogOpen(false)}
+        />
         <input
             className="hidden"
             type="file"
@@ -148,8 +161,50 @@ export default function Page() {
                         <Link className={css.secondary} href="/edit/password">
                             <span>비밀번호 변경</span>
                         </Link>
-                        <button className={css.secondary} onClick={() => {
+                        <button className={css.secondary} onClick={async () => {
+                            try {
+                                const flag = await new Promise<boolean>((res) => {
+                                    setDialogTitle("회원탈퇴");
+                                    setDialogDesc("정말로 탈퇴하겠습니까?\n탈퇴하면 Just Chatting에서의 모든 데이터가 사라집니다.");
+                                    setDialogButtons([
+                                        {
+                                            "text": "취소",
+                                            "onClick": () => {
+                                                setDialogOpen(false);
+                                                res(false);
+                                            }
+                                        },
+                                        {
+                                            "text": "탈퇴하기",
+                                            "onClick": () => {
+                                                setDialogOpen(false);
+                                                res(true);
+                                            }
+                                        }
+                                    ]);
+                                    setDialogOpen(true);
+                                });
 
+                                if (!flag) return;
+
+                                const r = await REST("/api/users/me", {
+                                    "method": "DELETE"
+                                });
+                                if (!r.success) {
+                                    throw new Error(r.data.message);
+                                }
+                                await REST("/api/auth/logout", {
+                                    "method": "POST"
+                                });
+
+                                localStorage.removeItem("access_token");
+                                localStorage.removeItem("expires_at");
+
+                                router.replace("/");
+                            } catch (err) {
+                                const e = err as Error;
+                                alert(e.message);
+                            }
                         }}>
                             <span>탈퇴하기</span>
                         </button>
