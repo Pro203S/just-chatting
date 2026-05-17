da653f74a8cfc4eed91a4bf00ad1460ac228644e
Pro203S <prosirea2501@gmail.com>
2026-05-17 22:29:40 +0900

    dialog 추가

---
+++ src/components/dialog/index.tsx
@@ -0,0 +1,98 @@
+import { useEffect } from 'react';
+import css from './styles.module.css';
+import { animated, easings, useSpringValue } from '@react-spring/web';
+
+export type DialogButton = {
+    "text": string,
+    "onClick"?: () => any
+}
+
+type Props = {
+    "title": string,
+    "description": string,
+    "buttons": DialogButton[],
+    "open": boolean,
+    "onCancel"?: () => any
+};
+
+const BACKGROUND_ANIMATE_CONFIG = {
+    "config": {
+        "duration": 375,
+        "easing": easings.easeOutQuad
+    }
+};
+
+const DIALOG_ANIMATE_CONFIG = {
+    "config": {
+        "duration": 375,
+        "easing": easings.easeOutCubic
+    }
+};
+
+export default function Dialog(props: Props) {
+    const { title, description, buttons, open, onCancel } = props;
+
+    const bgOpacity = useSpringValue(0, BACKGROUND_ANIMATE_CONFIG);
+    const bgBlur = useSpringValue(0, BACKGROUND_ANIMATE_CONFIG);
+
+    const dialogOpacity = useSpringValue(0, DIALOG_ANIMATE_CONFIG);
+    const dialogY = useSpringValue(10, DIALOG_ANIMATE_CONFIG);
+
+    useEffect(() => {
+        if (open) {
+            bgOpacity.start(1);
+            bgBlur.start(5);
+
+            dialogOpacity.start(1);
+            dialogY.start(0);
+        } else {
+            bgOpacity.start(0);
+            bgBlur.start(0);
+
+            dialogOpacity.start(0);
+            dialogY.start(10);
+        }
+    }, [open]);
+
+    useEffect(() => {
+        const cb = ({ key }: KeyboardEvent) => {
+            if (key !== "Escape") return;
+
+            onCancel?.();
+        };
+
+        document.addEventListener("keydown", cb);
+
+        return () => document.removeEventListener("keydown", cb);
+    }, []);
+
+    return <animated.div
+        className={css.background}
+        onClick={({ target, currentTarget }) => {
+            if (target !== currentTarget) return;
+
+            onCancel?.();
+        }}
+        style={{
+            "pointerEvents": open ? "auto" : "none",
+            "backdropFilter": bgBlur.to(v => `blur(${v}px)`)
+        }}>
+        <animated.div
+            className={css.dialog}
+            style={{
+                "opacity": dialogOpacity,
+                "transform": dialogY.to(v => `translateY(${v}px)`)
+            }}
+        >
+            <div className={css.texts}>
+                <span className={css.title}>{title}</span>
+                <span className={css.description}>{description}</span>
+            </div>
+            <div className={css.buttons}>
+                {buttons.map(v => <button key={v.text} className={css.button} onClick={v.onClick}>
+                    <span>{v.text}</span>
+                </button>)}
+            </div>
+        </animated.div>
+    </animated.div>;
+}
